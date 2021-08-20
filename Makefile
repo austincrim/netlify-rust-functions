5360fee45bc465d0db803257717c51a4f862709d
Austin Crim <crim.austin@principal.com>
2021-08-20 11:22:02 -0500

    new structure

--- Makefile
+++ Makefile
@@ -2,8 +2,7 @@
 
 build: ## Build static binary and put it in the functions directory.
 	@cargo build --release --target x86_64-unknown-linux-musl
-	@mkdir -p functions
-	@cp target/x86_64-unknown-linux-musl/release/hello-rs functions
+	@cp target/x86_64-unknown-linux-musl/release/hello-rs functions/hello-rs
 
 deploy: build ## Deploy the site using Netlify's CLI
 	@netlify deploy --prod
--- src/main.rs
+++ src/main.rs
@@ -7,7 +7,7 @@ type Error = Box<dyn std::error::Error + Send + Sync + 'static>;
 
 #[lambda(http)]
 #[tokio::main]
-async fn main(req: Request, _: Context) -> Result<impl IntoResponse, Error> {
-    let file = std::fs::read_to_string("./views/hello.html").expect("path not found!");
+async fn main(_: Request, _: Context) -> Result<impl IntoResponse, Error> {
+    let file = std::fs::read_to_string("./hello.html").expect("path not found!");
     Ok(format!("{}", file))
 }
